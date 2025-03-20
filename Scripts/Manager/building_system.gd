1a99a58f1211b0aa187ee2a166ce7ac97ac2d92c
YmirWork <42849379+ymirwork@users.noreply.github.com>
2025-03-21 00:20:07 +0100

    #3
    Working Z index

--- Scripts/Manager/building_system.gd
+++ Scripts/Manager/building_system.gd
@@ -5,6 +5,7 @@ class_name BuildingSystem
 @onready var buildable: TileMapLayer = $"../World/buildable"
 var selected_tile: int = 1 # Defualt none tile
 var is_building: bool = false
+var tile_array: Array
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,16 +15,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 func select_building(tile_id: int):
 	selected_tile = tile_id
 	is_building = true
 	
 func can_build_tile(cell_position: Vector2i):
 	if selected_tile != -1:
-		buildable.set_cell(cell_position, 0, Vector2i(0,0))
-		buildable.cell
-		print(cell_position)
+		buildable.set_cell(cell_position, 2, Vector2i(0,0),1)
+		print(buildable.get_surrounding_cells(cell_position))
+		
+		
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch and event.is_pressed():
@@ -32,5 +33,6 @@ func _input(event: InputEvent) -> void:
 		
 		
 		if can_build_tile(cell_pos):
-			print("YES")
+			#print(buildable.get_surrounding_cells(cell_pos))
+			pass
 		
---
+++ Scripts/kafelek.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+var position_kafelek:
+	set(value):
+		value = position
+	get:
+		return position
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	z_index += position_kafelek.y
+	print("Kafelek:", position_kafelek, "Index Z : ", z_index)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
