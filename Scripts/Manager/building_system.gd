7073e4c54d13a45fa528ce0f403fd98b27481f72
YmirWork <42849379+ymirwork@users.noreply.github.com>
2025-03-20 23:38:59 +0100

    #2

--- Scripts/Manager/building_system.gd
+++ Scripts/Manager/building_system.gd
@@ -3,6 +3,8 @@ class_name BuildingSystem
 
 
 @onready var buildable: TileMapLayer = $"../World/buildable"
+var selected_tile: int = 1 # Defualt none tile
+var is_building: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,14 +13,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var mouse_position = get_global_mouse_position()
+	pass
 
-
-
-func check_tile():
-	return buildable
+func select_building(tile_id: int):
+	selected_tile = tile_id
+	is_building = true
+	
+func can_build_tile(cell_position: Vector2i):
+	if selected_tile != -1:
+		buildable.set_cell(cell_position, 0, Vector2i(0,0))
+		buildable.cell
+		print(cell_position)
 
 func _input(event: InputEvent) -> void:
-	if event == InputEventScreenTouch and event.is_pressed():
-		print("PRESS")
+	if event is InputEventScreenTouch and event.is_pressed():
+		var position_tile = event.position
+		var cell_pos = buildable.local_to_map(position_tile)
+		
+		
+		if can_build_tile(cell_pos):
+			print("YES")
 		
