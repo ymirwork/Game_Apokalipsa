52adbea62f15df849227abd515e879866d643bba
YmirWork <42849379+ymirwork@users.noreply.github.com>
2025-03-21 14:27:35 +0100

    #4

--- Scripts/Manager/building_system.gd
+++ Scripts/Manager/building_system.gd
@@ -1,38 +1,44 @@
 extends Node2D
-class_name BuildingSystem
+class_name Building_System
 
 
-@onready var buildable: TileMapLayer = $"../World/buildable"
-var selected_tile: int = 1 # Defualt none tile
-var is_building: bool = false
-var tile_array: Array
+@onready var buildable: TileMapLayer = $"../World/Buildable"
+@onready var preview: TileMapLayer = $"../World/Preview"
+
+var source_id : int
+var selected_tile: Vector2i
+var select_mode: bool = false
+var preview_tile: Vector2i:
+	set(value):
+		if preview_tile == value:
+			return
+		preview.erase_cell(preview_tile)
+		preview_tile = value
+		preview.set_cell(value, scene_tile_building, Vector2i(0,0),building_id)
+
+var scene_tile_building: int = 2 # later adding from Building_Resources
+var building_id: int = 1
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-func select_building(tile_id: int):
-	selected_tile = tile_id
-	is_building = true
-	
-func can_build_tile(cell_position: Vector2i):
-	if selected_tile != -1:
-		buildable.set_cell(cell_position, 2, Vector2i(0,0),1)
-		print(buildable.get_surrounding_cells(cell_position))
-		
-		
+func get_snapped_grid_position(pos: Vector2) -> Vector2i:
+	var local_pos = buildable.to_local(pos)
+	var tile_pos = buildable.local_to_map(local_pos)
+	return tile_pos
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch and event.is_pressed():
-		var position_tile = event.position
-		var cell_pos = buildable.local_to_map(position_tile)
+	#PC DEBUG
+	if event is InputEventKey:
+		if event.keycode == KEY_1 and event.pressed: # Clicked 1 to active building for debug. #Later add UI
+			select_mode = true
+	
+	if event is InputEventScreenTouch and event.is_pressed() and select_mode:
+		if select_mode:
+			preview_tile = get_snapped_grid_position(event.position)
+			print(preview_tile)
+			place_building(preview_tile)
+			select_mode = false
 		
-		
-		if can_build_tile(cell_pos):
-			#print(buildable.get_surrounding_cells(cell_pos))
-			pass
-		
+
+func place_building(building_pos: Vector2i):
+	buildable.set_cell(building_pos, scene_tile_building, Vector2i(0,0),building_id)
+	preview.erase_cell(building_pos)
---
+++ Scripts/Resources/Building_Resources.gd
@@ -0,0 +1,6 @@
+extends Resource 
+class_name Building_Resources
+
+
+@export var name: String
+@export var source_id: int
